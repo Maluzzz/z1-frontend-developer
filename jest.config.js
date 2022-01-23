ab0751239daa3d46fa2ccfd784c4b6ec9688b2cc
Maluzzz <maaluuz@gmail.com>
2022-01-23 13:18:42 +0100

    Allow Jest work with svg, to test Label component

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   transform: {
-     '^.+\\.(ts|js|html|svg|tsx)$': "ts-jest",
-     "\\.svg$": "svg-jest"
+    '^.+\\.(ts|js|html|svg|tsx)$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/__mocks__/svg.js',
+  },
 }
--- src/components/Label/test.tsx
+++ src/components/Label/test.tsx
@@ -1,16 +1,20 @@
+/* eslint-disable no-undef */
+import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 import { Label } from './index'
 
 describe('Testing component Label', () => {
-  test('should return the component with danger type', () => {
-    return
+  test('should return the component without crashing', () => {
+    render(<Label type='danger' text='Accepted' />)
+    const label = screen.getByAltText('icon')
+    const path = label.getAttribute('src')
+    expect(screen.getByText('Accepted')).toBeInTheDocument()
+    expect(path).toContain('SvgrURL')
   })
-  test('should return the component with success type', () => {
-    return
-  })
-  test('should return the empty component', () => {
-    return
+  test('should return the component empty component', () => {
+    render(<Label type='no-exist' text='Accepted' />)
+    // TO.DO
   })
 })
